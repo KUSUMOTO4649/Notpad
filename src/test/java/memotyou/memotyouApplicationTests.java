bcd84c5120170d40b0140bcfc91f57937cd30ceb
User <c.c.kenken2023@gmail.com>
2023-11-02 10:45:31 +0900

    java制作中???

---
+++ src/test/java/memotyou/memotyouApplicationTests.java
@@ -0,0 +1,13 @@
+package memotyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class memotyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
